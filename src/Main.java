1a0922b2ff3d2f78b3be35854d37cfb4c92717ab
Vlad <vladifoki@gmail.com>
2023-03-29 20:52:05 +0300

    Install

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int intOdin = 8;
         int intDva = intOdin + 3;
-        int intTri = intOdin+ intDva;
+        int intTri = intOdin + intDva;
     }
 }
